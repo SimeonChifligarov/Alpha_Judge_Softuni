eb03aab2068146107d7de4289522e10ff8b39171
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-23 11:07:45 +0200

    add 06_Operations_Between_Numbers.py

---
+++ Python_Basics/PB_Python/03_02_Conditional_Statements_Advanced_Exercise/06_Operations_Between_Numbers.py
@@ -0,0 +1,25 @@
+num1 = int(input())
+num2 = int(input())
+operator = input()
+
+if operator == '+':
+    result = num1 + num2
+    print(f'{num1} + {num2} = {result} - {"even" if result % 2 == 0 else "odd"}')
+elif operator == '-':
+    result = num1 - num2
+    print(f'{num1} - {num2} = {result} - {"even" if result % 2 == 0 else "odd"}')
+elif operator == '*':
+    result = num1 * num2
+    print(f'{num1} * {num2} = {result} - {"even" if result % 2 == 0 else "odd"}')
+elif operator == '/':
+    if num2 == 0:
+        print(f'Cannot divide {num1} by zero')
+    else:
+        result = num1 / num2
+        print(f'{num1} / {num2} = {result:.2f}')
+elif operator == '%':
+    if num2 == 0:
+        print(f'Cannot divide {num1} by zero')
+    else:
+        result = num1 % num2
+        print(f'{num1} % {num2} = {result}')
