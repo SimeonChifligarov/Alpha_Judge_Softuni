422bc2d7cb90f0a67ba8a7ff7eab6d453adbe9be
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-26 09:36:27 +0200

    add 02_Password_wT.py

---
+++ Python_Basics/PB_Python/05_01_While_Loop_Lab/02_Password_wT.py
@@ -0,0 +1,10 @@
+# using while True
+
+username = input()
+password = input()
+
+while True:
+    entered_password = input()
+    if entered_password == password:
+        print(f'Welcome {username}!')
+        break
