cba760e6b81ba8149a7282dae8053ddcca0a0b01
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-26 09:37:20 +0200

    add 04_Sequence_2k_plus_1_wT.py

---
+++ Python_Basics/PB_Python/05_01_While_Loop_Lab/04_Sequence_2k_plus_1_wT.py
@@ -0,0 +1,10 @@
+# using while True
+
+boundary_number = int(input())
+
+next_number = 1
+while True:
+    if next_number > boundary_number:
+        break
+    print(next_number)
+    next_number = next_number * 2 + 1
