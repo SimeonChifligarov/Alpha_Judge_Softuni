a6558898a0747888f14860dcefe77ce2b103cfc8
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-11-28 09:45:08 +0200

    add 01_Old_Books.py

---
+++ Python_Basics/PB_Python/05_02_While_Loop_Exercise/01_Old_Books.py
@@ -0,0 +1,38 @@
+# # Solution 1
+# checked_books = 0
+# target_book = input()
+#
+#
+# command = input()
+# while command != 'No More Books':
+#     current_book = command
+#     if current_book == target_book:
+#         break
+#
+#     checked_books += 1
+#     command = input()
+#
+# if command != 'No More Books':
+#     print(f'You checked {checked_books} books and found it.')
+# else:
+#     print('The book you search is not here!')
+#     print(f'You checked {checked_books} books.')
+#
+# Solution 2
+NO_MORE_BOOKS_COMMAND = 'No More Books'
+
+checked_books = 0
+target_book = input()
+
+command = input()
+while not command == NO_MORE_BOOKS_COMMAND:
+    current_book = command
+    if current_book == target_book:
+        print(f'You checked {checked_books} books and found it.')
+        break
+
+    checked_books += 1
+    command = input()
+else:
+    print('The book you search is not here!')
+    print(f'You checked {checked_books} books.')
