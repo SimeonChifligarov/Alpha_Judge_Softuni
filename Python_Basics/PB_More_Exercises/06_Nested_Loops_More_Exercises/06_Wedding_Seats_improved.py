5d8e340481035e25ec23fbd6b458e6a8b686b19c
SimeonChifligarov <simeon.chifligarov@gmail.com>
2024-12-15 17:05:39 +0200

    add 06_Wedding_Seats_improved.py

---
+++ Python_Basics/PB_More_Exercises/06_Nested_Loops_More_Exercises/06_Wedding_Seats_improved.py
@@ -0,0 +1,21 @@
+result = []
+
+last_sector = input()
+rows_first_count = int(input())
+seats_odd_count = int(input())
+
+current_rows = rows_first_count
+for s in range(ord('A'), ord(last_sector) + 1):
+    current_sector = chr(s)
+    for c_r in range(1, current_rows + 1):
+        current_seats = seats_odd_count if c_r % 2 != 0 else seats_odd_count + 2
+
+        for c_s in range(1, current_seats + 1):
+            current_seat = chr(c_s + ord('a') - 1)
+            result_str = f'{current_sector}{c_r}{current_seat}'
+            result.append(result_str)
+
+    current_rows += 1
+
+print('\n'.join(result))
+print(len(result))
