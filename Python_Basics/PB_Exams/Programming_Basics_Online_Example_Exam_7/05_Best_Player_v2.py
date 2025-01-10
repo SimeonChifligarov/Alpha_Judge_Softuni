c95b9c3f342b811c0e8adbd7995901bfc321e7ed
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-01-10 09:31:43 +0200

    add 05_Best_Player_v2.py

---
+++ Python_Basics/PB_Exams/Programming_Basics_Online_Example_Exam_7/05_Best_Player_v2.py
@@ -0,0 +1,26 @@
+def find_best_player():
+    best_player = ''
+    max_goals = 0
+
+    while True:
+        player = input()
+        if player == 'END':
+            break
+        goals = int(input())
+
+        if goals > max_goals:
+            best_player = player
+            max_goals = goals
+
+        if goals >= 10:
+            break
+
+    print(f'{best_player} is the best player!')
+    if max_goals >= 3:
+        print(f'He has scored {max_goals} goals and made a hat-trick !!!')
+    else:
+        print(f'He has scored {max_goals} goals.')
+
+
+if __name__ == '__main__':
+    find_best_player()
