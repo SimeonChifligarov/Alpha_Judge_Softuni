19c7b8732258af000e6f544f4da65a277b6270f9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-16 09:55:16 +0200

    add tezt.py

---
+++ tezt.py
@@ -0,0 +1,13 @@
+# a = [1, 2]
+# b = a.copy()
+#
+# print(a==b)
+# print(a is b)
+
+print(*range(13, 5, -1), sep='; ')
+print(list(range(13, 5, -1)))
+
+# negative_number = -1234
+# for d in str(abs(negative_number)):
+#     print(int(d) * 1000)
+
