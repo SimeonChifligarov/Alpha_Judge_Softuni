359755c4054f34f71c5d5922532cf42ecc90e4c4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-17 13:03:08 +0200

    add 07_Rounding.py

---
+++ Python_Fundamentals/Python_Fundamentals/04_01_Functions_Lab/07_Rounding.py
@@ -0,0 +1,6 @@
+list_of_numbers = [float(el) for el in input().split()]
+list_of_rounded_numbers = [round(num) for num in list_of_numbers]
+
+print(list_of_rounded_numbers)
+
+# print([round(float(n)) for n in input().split()])
