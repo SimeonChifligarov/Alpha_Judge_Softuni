0f9dca47b5b8e69d63d284fe89a2e37606b77c6b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-21 17:07:41 +0200

    add 12_Factorial_Division_v2.py

---
+++ Python_Fundamentals/Python_Fundamentals/04_02_Functions_Exercise/12_Factorial_Division_v2.py
@@ -0,0 +1,18 @@
+# Write a function that receives two integer numbers.
+# Calculate factorial of each number. Divide the first result by the second and
+# print the division formatted to the second decimal point.
+
+def factorial_of_num(num):
+    result = 1
+    for num in range(1, num + 1):
+        result *= num
+    return result
+
+
+first_number = int(input())
+second_number = int(input())
+
+first_number_factorial = factorial_of_num(first_number)
+second_number_factorial = factorial_of_num(second_number)
+
+print(f'{(first_number_factorial / second_number_factorial):.2f}')
