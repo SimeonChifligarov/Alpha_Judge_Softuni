61377acc0722a765656ebe41044c2316c8897cb1
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-02-28 18:50:27 +0200

    add 04_Number_Classification_v3.py

---
+++ Python_Fundamentals/Python_Fundamentals/05_02_Lists_Advanced_Exercise/04_Number_Classification_v3.py
@@ -0,0 +1,23 @@
+def categorize_numbers(numbers: list[int]) -> tuple[list[int], list[int], list[int], list[int]]:
+    """
+    Categorizes numbers into positive, negative, even, and odd.
+
+    :param numbers: List of integers.
+    :return: Tuple containing four lists: positive, negative, even, and odd numbers.
+    """
+    positives = [num for num in numbers if num >= 0]
+    negatives = [num for num in numbers if num < 0]
+    evens = [num for num in numbers if num % 2 == 0]
+    odds = [num for num in numbers if num % 2 != 0]
+
+    return positives, negatives, evens, odds
+
+
+if __name__ == '__main__':
+    input_numbers = [int(el) for el in input().split(', ')]
+    pos, neg, even, odd = categorize_numbers(numbers=input_numbers)
+
+    print(f'Positive: {", ".join(map(str, pos))}')
+    print(f'Negative: {", ".join(map(str, neg))}')
+    print(f'Even: {", ".join(map(str, even))}')
+    print(f'Odd: {", ".join(map(str, odd))}')
