1f7ee6e8f5c52b03439edca172a625a055a6a4c9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-10 09:05:31 +0200

    add 03_Statistics.py

---
+++ Python_Fundamentals/Python_Fundamentals/07_01_Dictionaries_Lab/03_Statistics.py
@@ -0,0 +1,19 @@
+# You will be receiving key-value pairs on separate lines separated by ': '
+# until you receive the command 'statistics'.
+
+data = input()
+products_dict = {}
+
+while not data == 'statistics':
+    product, quantity = data.split(': ')
+    quantity = int(quantity)
+    if product not in products_dict:
+        products_dict[product] = 0
+    products_dict[product] += quantity
+    data = input()
+
+print('Products in stock:')
+for prod, quantity in products_dict.items():
+    print(f'- {prod}: {quantity}')
+print(f'Total Products: {len(products_dict)}')
+print(f'Total Quantity: {sum(products_dict.values())}')
