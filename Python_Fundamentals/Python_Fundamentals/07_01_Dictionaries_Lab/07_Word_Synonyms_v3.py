31708bea3bbbc5416495165a2ba25fdc7dd09e26
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-11 08:25:05 +0200

    add 07_Word_Synonyms_v3.py

---
+++ Python_Fundamentals/Python_Fundamentals/07_01_Dictionaries_Lab/07_Word_Synonyms_v3.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+
+def collect_synonyms(n: int) -> dict[str, list[str]]:
+    """Collects synonyms for words from user input."""
+    synonyms_dict = defaultdict(list)
+
+    for _ in range(n):
+        word = input()
+        synonym = input()
+        synonyms_dict[word].append(synonym)
+
+    return synonyms_dict
+
+
+def format_synonyms_output(synonyms: dict[str, list[str]]) -> str:
+    """Formats the synonyms dictionary into the required output format."""
+    return '\n'.join(f"{word} - {', '.join(syn_list)}" for word, syn_list in synonyms.items())
+
+
+if __name__ == '__main__':
+    count = int(input())
+    synonyms_dictionary = collect_synonyms(n=count)
+    print(format_synonyms_output(synonyms=synonyms_dictionary))
