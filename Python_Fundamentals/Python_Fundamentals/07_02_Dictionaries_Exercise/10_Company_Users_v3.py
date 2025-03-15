b9774c61cfa1b2b35d531ddfd0a386d85570c7b4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-15 11:22:50 +0200

    add 10_Company_Users_v3.py

---
+++ Python_Fundamentals/Python_Fundamentals/07_02_Dictionaries_Exercise/10_Company_Users_v3.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+def register_employees() -> dict[str, list[str]]:
+    """
+    Reads company names and employee IDs from input.
+    Returns a dictionary where keys are company names and values are lists of unique employee IDs, maintaining insertion order.
+    """
+    companies = defaultdict(list)
+
+    while True:
+        data = input()
+        if data == 'End':
+            break
+
+        company_name, employee_id = data.split(' -> ', maxsplit=1)
+
+        if employee_id not in companies[company_name]:
+            companies[company_name].append(employee_id)
+
+    return companies
+
+
+def print_companies(companies: dict[str, list[str]]) -> None:
+    """
+    Prints each company name and its employees' IDs.
+    """
+    for company, employees in companies.items():
+        print(company)
+        for employee in employees:
+            print(f'-- {employee}')
+
+
+if __name__ == '__main__':
+    company_data = register_employees()
+    print_companies(companies=company_data)
