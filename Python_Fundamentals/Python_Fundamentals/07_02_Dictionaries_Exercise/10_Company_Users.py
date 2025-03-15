5f661af1dd14eb8feec51ec22913a35c4e877145
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-15 11:22:29 +0200

    add 10_Company_Users.py

---
+++ Python_Fundamentals/Python_Fundamentals/07_02_Dictionaries_Exercise/10_Company_Users.py
@@ -0,0 +1,30 @@
+# •	Until you receive the 'End' command, you will be receiving input
+# in the format: '{companyName} -> {employeeId}'.
+# •	The input always will be valid.
+
+company_employee = input()
+companies_dict = {}
+
+while not company_employee == 'End':
+    # company = company_employee.split(' -> ')[0]
+    # employee = company_employee.split(' -> ')[1]
+    company, employee = company_employee.split(' -> ', maxsplit=1)
+    if company not in companies_dict:
+        companies_dict[company] = []
+    if employee not in companies_dict[company]:
+        companies_dict[company].append(employee)
+
+    company_employee = input()
+
+# sorted_companies_dict = sorted(companies_dict.items(), key=lambda x: x[0])
+#
+# for company, employees in sorted_companies_dict:
+#     print(f'{company}')
+#     for employee in employees:
+#         print(f'-- {employee}')
+
+
+for company, employees in companies_dict.items():
+    print(f'{company}')
+    for employee in employees:
+        print(f'-- {employee}')
