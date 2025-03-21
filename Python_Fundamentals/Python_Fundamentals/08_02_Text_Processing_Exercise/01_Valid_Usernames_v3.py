ba50d3c9171088c6943e280077a88923dc73c1c4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-21 10:20:06 +0200

    add 01_Valid_Usernames_v3.py

---
+++ Python_Fundamentals/Python_Fundamentals/08_02_Text_Processing_Exercise/01_Valid_Usernames_v3.py
@@ -0,0 +1,22 @@
+def is_valid_username(username: str) -> bool:
+    """
+    Checks if the given username is valid.
+    A valid username:
+    - Has a length between 3 and 16 characters inclusive
+    - Contains only letters, numbers, hyphens, and underscores
+    - Has no redundant symbols before, after, or in between
+    """
+    return 3 <= len(username) <= 16 and username.replace('-', '').replace('_', '').isalnum()
+
+
+def filter_valid_usernames(usernames: list[str]) -> list[str]:
+    """
+    Filters and returns a list of valid usernames from the given input string.
+    """
+    return [username for username in usernames if is_valid_username(username)]
+
+
+if __name__ == '__main__':
+    usernames_input: list[str] = input().split(', ')
+    valid_usernames: list[str] = filter_valid_usernames(usernames=usernames_input)
+    print('\n'.join(valid_usernames))
