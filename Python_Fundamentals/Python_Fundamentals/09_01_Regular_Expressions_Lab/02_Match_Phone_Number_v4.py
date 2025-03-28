945f0c37c5be354fda4095f4a44d1f571ffbe90b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-03-28 15:08:41 +0200

    add 02_Match_Phone_Number_v4.py

---
+++ Python_Fundamentals/Python_Fundamentals/09_01_Regular_Expressions_Lab/02_Match_Phone_Number_v4.py
@@ -0,0 +1,18 @@
+import re
+
+
+def match_sofia_phone_numbers(text: str) -> list[str]:
+    """
+    Matches valid Sofia phone numbers from a given text.
+
+    :param text: The input text containing phone numbers and other symbols.
+    :return: A list of matched phone numbers.
+    """
+    regex_pattern = r'(^|(?<=\s))\+359( |-)2\2\d{3}\2\d{4}\b'
+    return [el.group() for el in re.finditer(regex_pattern, text)]
+
+
+if __name__ == '__main__':
+    input_text = input()
+    matched_numbers = match_sofia_phone_numbers(text=input_text)
+    print(', '.join(matched_numbers))
