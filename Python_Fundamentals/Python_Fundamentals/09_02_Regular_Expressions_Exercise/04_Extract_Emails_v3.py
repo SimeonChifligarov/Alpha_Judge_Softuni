c16adde4f12fffdf120b94bdb954f0090c9a554e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-04-01 10:22:38 +0300

    add 04_Extract_Emails_v3.py

---
+++ Python_Fundamentals/Python_Fundamentals/09_02_Regular_Expressions_Exercise/04_Extract_Emails_v3.py
@@ -0,0 +1,17 @@
+import re
+
+
+def extract_emails(text: str) -> list[str]:
+    """
+    Extracts all valid email addresses from the given text and returns them as a list.
+    """
+    pattern = r'(?:(?<=\s)|(?<=^))[a-zA-Z0-9]+(?:[\._-][a-zA-Z0-9]+)*@[a-zA-Z]+(?:-[a-zA-Z]+)?(?:\.[a-zA-Z]+(?:-[a-zA-Z]+)?)+'  # noqa
+    return [el.group() for el in re.finditer(pattern, text)]
+
+
+if __name__ == '__main__':
+    input_text = input().strip()
+
+    result = extract_emails(text=input_text)
+    for email in result:
+        print(email)
