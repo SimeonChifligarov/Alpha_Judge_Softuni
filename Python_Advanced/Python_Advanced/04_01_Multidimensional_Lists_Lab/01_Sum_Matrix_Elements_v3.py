b7d3effcd750332cb645c8af3a380a148a10f36e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-04-19 09:09:07 +0300

    add 01_Sum_Matrix_Elements_v3.py

---
+++ Python_Advanced/Python_Advanced/04_01_Multidimensional_Lists_Lab/01_Sum_Matrix_Elements_v3.py
@@ -0,0 +1,23 @@
+def get_matrix_sum_and_display(data: list[list[int]]) -> tuple[int, list[list[int]]]:
+    """
+    This function calculates the sum of all numbers in a matrix.
+
+    Args:
+    rows: number of rows in the matrix
+    columns: number of columns in the matrix
+    data: list of lists, where each inner list is a row of integers
+
+    Returns:
+    The sum of all numbers and the matrix itself
+    """
+    total_sum = sum([element for row in data for element in row])
+    return total_sum, data
+
+
+if __name__ == '__main__':
+    rows_count, columns_count = [int(x) for x in input().split(', ')]
+    matrix_data = [[int(x) for x in input().split(', ')] for _ in range(rows_count)]
+
+    result_sum, result_matrix = get_matrix_sum_and_display(data=matrix_data)
+    print(result_sum)
+    print(result_matrix)
