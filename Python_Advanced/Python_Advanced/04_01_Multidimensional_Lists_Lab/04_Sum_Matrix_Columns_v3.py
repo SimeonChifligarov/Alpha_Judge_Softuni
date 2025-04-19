2ae0b277dbb70c1b1b640e7af81228441652e09b
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-04-19 09:10:20 +0300

    add 04_Sum_Matrix_Columns_v3.py

---
+++ Python_Advanced/Python_Advanced/04_01_Multidimensional_Lists_Lab/04_Sum_Matrix_Columns_v3.py
@@ -0,0 +1,25 @@
+def sum_columns(row_count: int, column_count: int, matrix_data: list[list[int]]) -> list[int]:
+    """
+    This function adds the numbers in each column of a matrix.
+
+    Args:
+    row_count: how many rows the matrix has
+    column_count: how many columns the matrix has
+    matrix_data: a list of lists with all numbers from the matrix
+
+    Returns:
+    A list where each number is the total of a column
+    """
+    column_sums = [sum(matrix_data[row][col] for row in range(row_count)) for col in range(column_count)]
+    return column_sums
+
+
+if __name__ == '__main__':
+    size_values = [int(x) for x in input().split(', ')]
+    row_total, column_total = size_values[0], size_values[1]
+
+    matrix_input = [[int(x) for x in input().split()] for _ in range(row_total)]
+
+    column_totals = sum_columns(row_count=row_total, column_count=column_total, matrix_data=matrix_input)
+    for total in column_totals:
+        print(total)
