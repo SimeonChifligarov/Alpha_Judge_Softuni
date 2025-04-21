39a88145d6daa3ff3601853faf3418a905600315
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-04-21 18:07:22 +0300

    add 02_Diagonal_Difference_v4.py

---
+++ Python_Advanced/Python_Advanced/04_02_Multidimensional_Lists_Exercise_1/02_Diagonal_Difference_v4.py
@@ -0,0 +1,23 @@
+def diagonal_difference(matrix_data: list[list[int]]) -> int:
+    """
+    This function finds the absolute difference between the two diagonals in a square matrix.
+
+    Args:
+    matrix_size: how many rows and columns the matrix has
+    matrix_data: a list of lists with matrix numbers
+
+    Returns:
+    The absolute difference between the diagonal sums
+    """
+    matrix_size = len(matrix_data)
+    primary_sum = sum(matrix_data[i][i] for i in range(matrix_size))
+    secondary_sum = sum(matrix_data[i][matrix_size - i - 1] for i in range(matrix_size))
+    return abs(primary_sum - secondary_sum)
+
+
+if __name__ == '__main__':
+    size_input = int(input())
+    matrix = [[int(x) for x in input().split()] for _ in range(size_input)]
+
+    difference = diagonal_difference(matrix_data=matrix)
+    print(difference)
