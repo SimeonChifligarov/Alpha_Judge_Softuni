cb4a887b2f65360a151c9c397ab8dd007a7e15e6
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-04-22 10:10:59 +0300

    add 04_Maximal_Sum_v2.py

---
+++ Python_Advanced/Python_Advanced/04_02_Multidimensional_Lists_Exercise_1/04_Maximal_Sum_v2.py
@@ -0,0 +1,50 @@
+def find_max_3x3_square(row_count: int, column_count: int, matrix_data: list[list[int]]) -> tuple[int, list[list[int]]]:
+    """
+    This function finds the 3x3 square in the matrix with the highest sum.
+
+    Args:
+    row_count: how many rows the matrix has
+    column_count: how many columns the matrix has
+    matrix_data: a list of lists with all the matrix numbers
+
+    Returns:
+    The highest sum and the corresponding 3x3 submatrix
+    """
+    max_sum = float('-inf')
+    best_submatrix = []
+
+    for row in range(row_count - 2):
+        for col in range(column_count - 2):
+            current_square = [
+                [matrix_data[row][col], matrix_data[row][col + 1], matrix_data[row][col + 2]],
+                [matrix_data[row + 1][col], matrix_data[row + 1][col + 1], matrix_data[row + 1][col + 2]],
+                [matrix_data[row + 2][col], matrix_data[row + 2][col + 1], matrix_data[row + 2][col + 2]]
+            ]
+            current_sum = sum([value for r in current_square for value in r])
+
+            if current_sum > max_sum:
+                max_sum = current_sum
+                best_submatrix = current_square
+
+    return max_sum, best_submatrix
+
+
+if __name__ == '__main__':
+    dimensions_input = input().split()
+    total_rows = int(dimensions_input[0])
+    total_columns = int(dimensions_input[1])
+
+    matrix = []
+    for _ in range(total_rows):
+        row_data = [int(x) for x in input().split()]
+        matrix.append(row_data)
+
+    max_total, max_square = find_max_3x3_square(
+        row_count=total_rows,
+        column_count=total_columns,
+        matrix_data=matrix
+    )
+
+    print(f'Sum = {max_total}')
+    for line in max_square:
+        print(*line)
