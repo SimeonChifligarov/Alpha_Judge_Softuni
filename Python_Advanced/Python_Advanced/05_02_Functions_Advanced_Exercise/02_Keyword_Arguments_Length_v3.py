95e4db655ae146e210826beb28d0f58fc998414f
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-01 11:45:38 +0300

    add 02_Keyword_Arguments_Length_v3.py

---
+++ Python_Advanced/Python_Advanced/05_02_Functions_Advanced_Exercise/02_Keyword_Arguments_Length_v3.py
@@ -0,0 +1,11 @@
+def kwargs_length(**info: str) -> int:
+    """
+    This function counts how many keyword arguments are given.
+
+    Args:
+        info: Any keyword arguments
+
+    Returns:
+        The number of keyword arguments
+    """
+    return len(info)
