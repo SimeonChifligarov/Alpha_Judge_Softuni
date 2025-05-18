9f866a9f2c385954960bdc9d7ce60e87bcfcc8a9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-18 11:10:41 +0300

    add 05_Flower_v3.py

---
+++ Python_Advanced/Python_OOP/01_02_First_Steps_in_OOP_Exercise/05_Flower_v3.py
@@ -0,0 +1,57 @@
+class Flower:
+    """
+    This class is for a flower. It needs some water to become happy.
+
+    Args:
+        name (str): The name of the flower
+        water_requirements (int): How much water the flower needs to be happy
+    """
+
+    def __init__(self, name: str, water_requirements: int) -> None:
+        self.name = name
+        self.water_requirements = water_requirements
+        self.is_happy = False
+
+    def water(self, quantity: int) -> None:
+        """
+        This method waters the flower. If enough water is given, it becomes happy.
+
+        Args:
+            quantity (int): The amount of water given
+        """
+        if quantity >= self.water_requirements:
+            self.is_happy = True
+
+    def status(self) -> str:
+        """
+        This method says if the flower is happy or not.
+
+        Returns:
+            str: The current mood of the flower
+        """
+        # return f"{self.name} is{' not' * (not self.is_happy)} happy"
+        # return f"{self.name} is{ {True: '', False: ' not'}[self.is_happy]} happy"
+        # return f"{self.name} is {'happy' if self.is_happy else 'not happy'}"
+        # return f"{self.name} is { 'not ' if not self.is_happy else '' }happy"
+        return f'{self.name} is happy' if self.is_happy else f'{self.name} is not happy'
+
+    def __str__(self) -> str:
+        return self.status()
+
+    def __repr__(self) -> str:
+        return f'Flower(name={self.name!r}, water_requirements={self.water_requirements!r}, is_happy={self.is_happy!r})'
+
+    def __bool__(self) -> bool:
+        return self.is_happy
+
+# if __name__ == '__main__':
+#     flower_name = 'Rose'
+#     required_water = 50
+#     flower_instance = Flower(name=flower_name, water_requirements=required_water)
+#     flower_instance.water(quantity=30)
+#     print(flower_instance.status())
+#     flower_instance.water(quantity=60)
+#     print(flower_instance.status())
+#     print(str(flower_instance))
+#     print(repr(flower_instance))
+#     print(bool(flower_instance))
