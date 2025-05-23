50d093821b8164f740a235158dbd7e02516e03f9
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-23 09:33:29 +0300

    add project 02_Single_Inheritance_adv

---
+++ Python_Advanced/Python_OOP/03_01_Inheritance_Lab/02_Single_Inheritance_adv/project/animal.py
@@ -0,0 +1,32 @@
+class Animal:
+    """
+    This class is about a simple animal that can eat.
+
+    Methods:
+        eat(): Says that the animal is eating.
+    """
+
+    # def eat(self) -> str:
+    #     return 'eating...'
+
+    @staticmethod
+    def eat() -> str:
+        return 'eating...'
+
+    def __str__(self) -> str:
+        return 'Animal()'
+
+    def __repr__(self) -> str:
+        return 'Animal()'
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Animal)
+
+    def __hash__(self) -> int:
+        return hash('Animal')
+
+
+if __name__ == '__main__':
+    # animal_instance = Animal()
+    # print(animal_instance.eat())
+    pass
