cd62e1d885eef2706805ab12b3ca28cbe1666994
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-28 10:57:46 +0300

    add 04_Email_Validator_v2.py

---
+++ Python_Advanced/Python_OOP/04_01_Encapsulation_Lab/04_Email_Validator_v2.py
@@ -0,0 +1,47 @@
+class EmailValidator:
+    """
+    This class is about an email validator that checks usernames, mails, and domains.
+
+    Args:
+        min_length (int): The minimum length of the username.
+        mails (list): The list of valid mails.
+        domains (list): The list of valid domains.
+    """
+
+    def __init__(self, min_length: int, mails: list[str], domains: list[str]) -> None:
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name: str) -> bool:
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail: str) -> bool:
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain: str) -> bool:
+        return domain in self.domains
+
+    def validate(self, email: str) -> bool:
+        if '@' not in email or '.' not in email:
+            return False
+        name, rest = email.split('@')
+        mail, domain = rest.split('.')
+        if not self.__is_name_valid(name):
+            return False
+        if not self.__is_mail_valid(mail):
+            return False
+        if not self.__is_domain_valid(domain):
+            return False
+        return True
+
+
+if __name__ == '__main__':
+    # mails_list = ['gmail', 'softuni']
+    # domains_list = ['com', 'bg']
+    # email_validator_instance = EmailValidator(min_length=6, mails=mails_list, domains=domains_list)
+    # print(email_validator_instance.validate(email='pe77er@gmail.com'))
+    # print(email_validator_instance.validate(email='georgios@gmail.net'))
+    # print(email_validator_instance.validate(email='stamatito@abv.net'))
+    # print(email_validator_instance.validate(email='abv@softuni.bg'))
+    pass
