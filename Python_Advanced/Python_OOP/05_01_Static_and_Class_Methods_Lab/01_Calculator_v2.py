0ec4a0d54ef9cd36ea9c1c7003497745e52df4d3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-05-31 21:49:03 +0300

    add 01_Calculator_v2.py

---
+++ Python_Advanced/Python_OOP/05_01_Static_and_Class_Methods_Lab/01_Calculator_v2.py
@@ -0,0 +1,41 @@
+class Calculator:
+    """This class helps to do simple math operations like add, multiply, divide and subtract."""
+
+    @staticmethod
+    def add(*values: float) -> float:
+        return sum(values)
+
+    @staticmethod
+    def multiply(*values: float) -> float:
+        result: float = 1
+        for element in values:
+            result *= element
+        return result
+
+    @staticmethod
+    def divide(*values: float) -> float:
+        if not values:
+            return 0
+        result: float = values[0]
+        for element in values[1:]:
+            if element == 0:
+                return 0
+            result /= element
+        return result
+
+    @staticmethod
+    def subtract(*values: float) -> float:
+        if not values:
+            return 0
+        result: float = values[0]
+        for element in values[1:]:
+            result -= element
+        return result
+
+
+if __name__ == '__main__':
+    # print(Calculator.add(5, 10, 4))
+    # print(Calculator.multiply(1, 2, 3, 5))
+    # print(Calculator.divide(100, 2))
+    # print(Calculator.subtract(90, 20, -50, 43, 7))
+    pass
