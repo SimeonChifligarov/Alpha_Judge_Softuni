a3c94f8759d99d8b4b473984b61b4c04992c8a83
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-12 20:20:26 +0300

    add 02_Dictionary_Iterator_v3.py

---
+++ Python_Advanced/Python_OOP/07_02_Iterators_and_Generators_Exercise/02_Dictionary_Iterator_v3.py
@@ -0,0 +1,39 @@
+class dictionary_iter:
+    """
+    This class makes an iterator that gives each key and value
+    from a dictionary as a tuple with two elements.
+
+    Args:
+        data (dict): The dictionary to iterate.
+    """
+
+    def __init__(self, data: dict) -> None:
+        self.data = list(data.items())
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        current_pair = self.data[self.index]
+        self.index += 1
+        return current_pair
+
+    def __str__(self) -> str:
+        return f'dictionary_iter(data={self.data})'
+
+    def __repr__(self) -> str:
+        return f'dictionary_iter(data={self.data})'
+
+
+if __name__ == '__main__':
+    # result_instance = dictionary_iter(data={1: '1', 2: '2'})
+    # for element in result_instance:
+    #     print(element)
+    #
+    # result_instance = dictionary_iter(data={'name': 'Peter', 'age': 24})
+    # for element in result_instance:
+    #     print(element)
+    pass
