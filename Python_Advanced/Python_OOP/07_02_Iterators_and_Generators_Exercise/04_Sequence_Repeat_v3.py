3bf152f5a407eee0b7e07697a32ae2f99f0b98be
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-13 06:22:26 +0300

    add 04_Sequence_Repeat_v3.py

---
+++ Python_Advanced/Python_OOP/07_02_Iterators_and_Generators_Exercise/04_Sequence_Repeat_v3.py
@@ -0,0 +1,45 @@
+class sequence_repeat:
+    """
+    This class makes an iterator that repeats a sequence
+    until it reaches a needed total number of characters.
+
+    Args:
+        sequence (str): The string to repeat.
+        count (int): How many characters to produce.
+    """
+
+    def __init__(self, sequence: str, count: int) -> None:
+        self.sequence = sequence
+        self.count = count
+        self.index = 0
+        self.sequence_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= self.count:
+            raise StopIteration
+        if self.sequence_index >= len(self.sequence):
+            self.sequence_index = 0
+        current_char = self.sequence[self.sequence_index]
+        self.sequence_index += 1
+        self.index += 1
+        return current_char
+
+    def __str__(self) -> str:
+        return f'sequence_repeat(sequence={self.sequence}, count={self.count})'
+
+    def __repr__(self) -> str:
+        return f'sequence_repeat(sequence={self.sequence}, count={self.count})'
+
+
+if __name__ == '__main__':
+    # result_instance = sequence_repeat(sequence='abc', count=5)
+    # for item in result_instance:
+    #     print(item, end='')
+    #
+    # result_instance = sequence_repeat(sequence='I Love Python', count=3)
+    # for item in result_instance:
+    #     print(item, end='')
+    pass
