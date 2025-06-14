f23c364a104279cc5b990263c74d73b989ca2cea
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-14 12:03:01 +0300

    add 08_Primer_Numbers_v2.py

---
+++ Python_Advanced/Python_OOP/07_02_Iterators_and_Generators_Exercise/08_Prime_Numbers_v2.py
@@ -0,0 +1,10 @@
+def is_prime(num):
+    return num > 1 and all(num % i for i in range(2, num))
+
+
+def get_primes(list_of_integers):
+    primes = filter(lambda n: is_prime(n), list_of_integers)
+    for pr in primes:
+        yield pr
+
+# print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
