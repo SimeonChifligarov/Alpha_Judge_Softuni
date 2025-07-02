b527b5c2805313e58270421949ac998ec95c41cb
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-07-02 17:39:33 +0300

    add 12_Table_Birthdays.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/01_02_Data_Types_and_Table_Basics_Exercise/12_Table_Birthdays.sql
@@ -0,0 +1,8 @@
+CREATE TABLE minions_birthdays (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL DEFAULT '',
+  date_of_birth DATE NOT NULL,
+  age INTEGER NOT NULL DEFAULT 0,
+  present VARCHAR(100) NOT NULL DEFAULT '',
+  party TIMESTAMPTZ NOT NULL
+);
