be50b7a32596bcae729e0f95c7fc5e068e99d0a8
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-07-24 09:13:54 +0300

    add 03_Customs.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/05_02_Table_Relations_Exercise/03_Customs.sql
@@ -0,0 +1,27 @@
+-- CREATE DATABASE customs_db;
+
+-- \c customs_db
+
+CREATE TABLE IF NOT EXISTS passports (
+    id INT GENERATED ALWAYS AS IDENTITY (START WITH 100 INCREMENT BY 1) PRIMARY KEY,
+    nationality VARCHAR(50)
+);
+
+INSERT INTO passports (nationality)
+VALUES
+    ('N34FG21B'),
+    ('K65LO4R7'),
+    ('ZE657QP2');
+
+CREATE TABLE IF NOT EXISTS people (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    salary NUMERIC(10, 2),
+    passport_id INT REFERENCES passports(id)
+);
+
+INSERT INTO people (first_name, salary, passport_id)
+VALUES
+    ('Roberto', 43300.00, 101),
+    ('Tom', 56100.00, 102),
+    ('Yana', 60200.00, 100);
