52d09f91e5656a9124c41d740b7f49bbfb9a6f83
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-07-27 10:25:35 +0300

    add 03_Employees_Projects.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/06_01_Subqueries_and_JOINs_Lab/03_Employees_Projects.sql
@@ -0,0 +1,9 @@
+SELECT
+    e.employee_id,
+    CONCAT(e.first_name, ' ', e.last_name) AS full_name,
+    p.project_id,
+    p.name AS project_name
+FROM employees e
+JOIN employees_projects ep ON e.employee_id = ep.employee_id
+JOIN projects p ON ep.project_id = p.project_id
+WHERE p.project_id = 1;
