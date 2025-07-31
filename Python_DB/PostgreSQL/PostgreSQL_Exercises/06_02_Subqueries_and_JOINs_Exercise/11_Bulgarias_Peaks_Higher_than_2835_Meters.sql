1d4cd1a1103b79727c185163122cfc63399a83c3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-07-31 19:03:59 +0300

    add 11_Bulgarias_Peaks_Higher_than_2835_Meters.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/06_02_Subqueries_and_JOINs_Exercise/11_Bulgarias_Peaks_Higher_than_2835_Meters.sql
@@ -0,0 +1,11 @@
+SELECT
+    mc.country_code,
+    m.mountain_range,
+    p.peak_name,
+    p.elevation
+FROM mountains_countries mc
+JOIN mountains m ON mc.mountain_id = m.id
+JOIN peaks p ON m.id = p.mountain_id
+WHERE mc.country_code = 'BG'
+  AND p.elevation > 2835
+ORDER BY p.elevation DESC;
