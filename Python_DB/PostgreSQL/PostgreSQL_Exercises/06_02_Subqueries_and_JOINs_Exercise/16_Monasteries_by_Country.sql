8210b5585995336021b657e9ecfd47bbe0638e92
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-02 08:38:01 +0300

    add 16_Monasteries_by_Country.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/06_02_Subqueries_and_JOINs_Exercise/16_Monasteries_by_Country.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS monasteries (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    monastery_name VARCHAR(255),
+    country_code CHAR(2)
+);
+
+INSERT INTO monasteries (monastery_name, country_code) VALUES
+('Rila Monastery "St. Ivan of Rila"', 'BG'),
+('Bachkovo Monastery "Virgin Mary"', 'BG'),
+('Troyan Monastery "Holy Mother''s Assumption"', 'BG'),
+('Kopan Monastery', 'NP'),
+('Thrangu Tashi Yangtse Monastery', 'NP'),
+('Shechen Tennyi Dargyeling Monastery', 'NP'),
+('Benchen Monastery', 'NP'),
+('Southern Shaolin Monastery', 'CN'),
+('Dabei Monastery', 'CN'),
+('Wa Sau Toi', 'CN'),
+('Lhunshigyia Monastery', 'CN'),
+('Rakya Monastery', 'CN'),
+('Monasteries of Meteora', 'GR'),
+('The Holy Monastery of Stavronikita', 'GR'),
+('Taung Kalat Monastery', 'MM'),
+('Pa-Auk Forest Monastery', 'MM'),
+('Taktsang Palphug Monastery', 'BT'),
+('Sümela Monastery', 'TR');
+
+ALTER TABLE countries
+ADD COLUMN three_rivers BOOLEAN DEFAULT FALSE;
+
+UPDATE countries
+SET three_rivers = TRUE
+WHERE country_code IN (
+    SELECT country_code
+    FROM countries_rivers
+    GROUP BY country_code
+    HAVING COUNT(river_id) > 3
+);
+
+SELECT
+    m.monastery_name,
+    c.country_name
+FROM monasteries m
+JOIN countries c USING (country_code)
+--WHERE NOT c.three_rivers  -- are we certain three_rivers is NOT NULL
+WHERE c.three_rivers = FALSE
+ORDER BY m.monastery_name ASC;
