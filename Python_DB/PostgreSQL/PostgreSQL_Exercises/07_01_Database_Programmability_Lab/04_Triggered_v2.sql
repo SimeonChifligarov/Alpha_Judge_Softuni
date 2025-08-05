5318c025ed121566f74560ff32aed49ea5eca933
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-05 09:29:14 +0300

    add 04_Triggered_v2.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/07_01_Database_Programmability_Lab/04_Triggered_v2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE  deleted_employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name varchar(20),
+    last_name varchar(20),
+    middle_name varchar(20),
+    job_title varchar(50),
+    department_id int,
+    salary numeric(19, 4)
+);
+
+CREATE or REPLACE FUNCTION backup_fired_employees()
+RETURNS TRIGGER
+AS
+$$
+    BEGIN
+        INSERT INTO deleted_employees (
+        first_name, last_name, middle_name, job_title, department_id, salary
+        )
+        VALUES (
+            old.first_name,
+            old.last_name,
+            old.middle_name,
+            old.job_title,
+            old.department_id,
+            old.salary
+        );
+        RETURN old;
+    END
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER  backup_employees_trigger
+AFTER DELETE on employees
+FOR EACH ROW EXECUTE PROCEDURE backup_fired_employees()
+;
