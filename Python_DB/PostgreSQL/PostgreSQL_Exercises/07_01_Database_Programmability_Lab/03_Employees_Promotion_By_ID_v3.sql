6fd7da10389d86f4fbc6238d4c00e9b42039c09e
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-05 09:29:00 +0300

    add 03_Employees_Promotion_By_ID_v3.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/07_01_Database_Programmability_Lab/03_Employees_Promotion_By_ID_v3.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE PROCEDURE sp_increase_salary_by_id(id INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF (SELECT employee_id FROM employees WHERE employee_id = id) IS NULL THEN
+        RETURN;
+    END IF;
+
+    UPDATE employees
+    SET salary = salary * 1.05
+    WHERE employee_id = id;
+END;
+$$;
