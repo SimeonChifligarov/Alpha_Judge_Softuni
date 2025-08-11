abfd70a765f169962ac79de21a6a68408e89c813
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-11 09:02:30 +0300

    add 12_Log_Accounts_Trigger.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exercises/07_02_Database_Programmability_Exercise/12_Log_Accounts_Trigger.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS logs (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    account_id INT,
+    old_sum NUMERIC(19,4),
+    new_sum NUMERIC(19,4)
+);
+
+CREATE OR REPLACE FUNCTION trigger_fn_insert_new_entry_into_logs()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO logs(account_id, old_sum, new_sum)
+    VALUES(OLD.id, OLD.balance, NEW.balance);
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER tr_account_balance_change
+AFTER UPDATE ON accounts
+FOR EACH ROW
+WHEN (OLD.balance IS DISTINCT FROM NEW.balance)
+EXECUTE FUNCTION trigger_fn_insert_new_entry_into_logs();
