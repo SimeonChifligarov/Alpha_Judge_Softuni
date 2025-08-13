a03e51e0e7b5d55f114bc1dbf773190e2d93a1c3
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-13 18:29:01 +0300

    add 03_07_Owners_and_Their_Animals.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exams/Exam_Preparation_I/03_07_Owners_and_Their_Animals.sql
@@ -0,0 +1,8 @@
+SELECT
+    o.name AS owner,
+    COUNT(a.id) AS count_of_animals
+FROM owners o
+JOIN animals a ON o.id = a.owner_id
+GROUP BY o.name
+ORDER BY count_of_animals DESC, owner ASC
+LIMIT 5;
