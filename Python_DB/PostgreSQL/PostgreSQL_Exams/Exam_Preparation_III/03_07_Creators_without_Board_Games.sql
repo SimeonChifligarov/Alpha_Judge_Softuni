8ac42522746139b09943c0d3364ba40874a621e2
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-08-18 08:38:22 +0300

    add 03_07_Creators_without_Board_Games.sql

---
+++ Python_DB/PostgreSQL/PostgreSQL_Exams/Exam_Preparation_III/03_07_Creators_without_Board_Games.sql
@@ -0,0 +1,8 @@
+SELECT
+    c.id,
+    c.first_name || ' ' || c.last_name AS creator_name,
+    c.email
+FROM creators c
+LEFT JOIN creators_board_games cbg ON c.id = cbg.creator_id
+WHERE cbg.board_game_id IS NULL
+ORDER BY creator_name ASC;
