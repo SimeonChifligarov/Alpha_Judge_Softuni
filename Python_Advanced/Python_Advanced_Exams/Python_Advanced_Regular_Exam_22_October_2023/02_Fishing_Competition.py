83cbad0f2029a9e9b10a73f2b9f74ba87d5e7cb2
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-09-10 19:43:37 +0300

    fix reading input

--- Python_Advanced/Python_Advanced_Exams/Python_Advanced_Regular_Exam_22_October_2023/02_Fishing_Competition.py
+++ Python_Advanced/Python_Advanced_Exams/Python_Advanced_Regular_Exam_22_October_2023/02_Fishing_Competition.py
@@ -11,10 +11,9 @@ for i in range(n):
 caught = 0
 dirs = {'up': (-1, 0), 'down': (1, 0), 'left': (0, -1), 'right': (0, 1)}
 
-for line in sys.stdin:
-    cmd = line.strip()
-    if not cmd:
-        continue
+lost = False
+while True:
+    cmd = input()
     if cmd == 'collect the nets':
         break
     dr, dc = dirs[cmd]
@@ -24,16 +23,18 @@ for line in sys.stdin:
     cell = area[r][c]
     if cell == 'W':
         print(f"You fell into a whirlpool! The ship sank and you lost the fish you caught. Last coordinates of the ship: [{r},{c}]")
-        sys.exit(0)
+        lost = True
+        break
     if cell.isdigit():
         caught += int(cell)
     area[r][c] = 'S'
 
-if caught >= 20:
-    print("Success! You managed to reach the quota!")
-else:
-    print(f"You didn't catch enough fish and didn't reach the quota! You need {20 - caught} tons of fish more.")
-if caught > 0:
-    print(f"Amount of fish caught: {caught} tons.")
-for row in area:
-    print(''.join(row))
+if not lost:
+    if caught >= 20:
+        print("Success! You managed to reach the quota!")
+    else:
+        print(f"You didn't catch enough fish and didn't reach the quota! You need {20 - caught} tons of fish more.")
+    if caught > 0:
+        print(f"Amount of fish caught: {caught} tons.")
+    for row in area:
+        print(''.join(row))
